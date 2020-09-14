b93c0da918125a42b9c2fa91d2885c62c777667d
Mohammad <55255167+mqureshi12@users.noreply.github.com>
2020-09-14 18:57:09 -0400

    made an additional require

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 app.listen(port, function () {
     console.log("Server Has Started!");
 });
