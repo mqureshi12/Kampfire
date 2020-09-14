b1bf3cabcc01a80229dc14e9fbd58a57ae113d2d
Mohammad <55255167+mqureshi12@users.noreply.github.com>
2020-09-14 19:39:39 -0400

    fixed app.js again

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb+srv://" + process.env.U_N + ":" + process.env.P_W + "@kampfire.uvcbx.mongodb.net/kamp_fire?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://mDev2231:BallPointPen3.0@kampfire.uvcbx.mongodb.net/kampfire?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
